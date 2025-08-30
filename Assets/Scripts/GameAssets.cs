75e8ec9868879028cd832fc93d3cddb143993678
Danh <cogdanh2k3@gmail.com>
2025-08-30 09:28:15 +0700

    Snake Core

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    #region
+    public static GameAssets instance;
+    #endregion
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    public Sprite snakeHeadSprite;
+    public Sprite foodSprite;
+}
---
+++ Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,50 @@
+using CodeMonkey;
+using UnityEngine;
+
+public class LevelGrid : MonoBehaviour
+{
+    private Vector2Int foodGridPosition;
+    private GameObject foodGameObject;
+    private int width;
+    private int height;
+    private Snake snake;
+
+    public LevelGrid(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+       
+    }
+
+    public void Setup(Snake snake)
+    {
+        this.snake = snake; 
+        SpawnFood();
+    }
+
+    private void SpawnFood()
+    {
+        do {
+            foodGridPosition = new Vector2Int(Random.Range(0, width), Random.Range(0, height));
+        } while (snake.GetFullSnakeGridPositionList().IndexOf(foodGridPosition) != -1);
+
+        foodGameObject = new GameObject("Food", typeof(SpriteRenderer));
+        foodGameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.instance.foodSprite;
+        foodGameObject.transform.position = new Vector3(foodGridPosition.x, foodGridPosition.y);
+    }
+
+    public bool TrySnakeEatFood(Vector2Int snakeGridPosition) { 
+        if(snakeGridPosition == foodGridPosition)
+        {
+            Object.Destroy(foodGameObject);
+            SpawnFood();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
